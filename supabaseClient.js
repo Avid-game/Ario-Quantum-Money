b65c2fddd24d73e0d7735939834bdad3c82bb65b
Avid-game <ebi123game@gmail.com>
2025-12-22 13:19:29 +0330

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-client'
 // آدرس اختصاصی دیتابیس آریو کوانتوم
 const supabaseUrl = 'https://ccusimbyepxhhjxbopbn.supabase.co'
 // کلید عمومی که قبلاً کپی کردی (بخش Publishable key)
-const supabaseAnonKey = sb_publishable_sqnNTDNwGoibZmFxqi57pg_nqtoK2TU
+const supabaseAnonKey = "sb_publishable_sqnNTDNwGoibZmFxqi57pg_nqtoK2TU"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
